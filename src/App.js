fa2ed90ad1214896d5366eb971f2686a23d8dc4b
jyotinadcab <jyotinadcab@gmail.com>
2026-04-23 17:55:57 +0530

    ll

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useLocation } from "react-router-dom";
 
 import Navbar from './components/Navbar';
 import ScrollToTop from "./components/ScrollToTop";
